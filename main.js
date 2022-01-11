0256bbd54ffd067494a8ba7238538ecd812dee80
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-11 21:47:05 +0530

    refactor: world manager

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 Utils.init();
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
+/** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
@@ -10,22 +11,13 @@ const ball = new Ball(50, 50, 20);
 const delta = 0.8;
 
 let i = 5;
+const world = new World();
 
-window.onload = update;
+world.add(ball);
 
+window.onload = world.render.bind(world);
 
-function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    if (i > 0) {
-        ball.force(0.6, VAR_GRAVITY);
-        i--;
-    }
-    ball.force(0, VAR_GRAVITY);
-    ball.verlet(delta);
-    ball.checkWall();
-    ball.render(ctx);
-    requestAnimationFrame(update);
-}
-
-
-// ball.render(ctx);
+window.addEventListener('click', (e) => {
+    const ball = new Ball(e.clientX, e.clientY, 20, 'rgba(27,155,244,0.3)');
+    world.add(ball);
+})
